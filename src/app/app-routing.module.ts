f131a545a6a9edc9576b9df8764546c8c1dc379d
Philip Gnann <philipgnann@aol.com>
2020-02-06 20:37:16 +0100

    added simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {BitcoinDetailsComponent} from "./bitcoin-details/bitcoin-details.component";
+import {BitcoinConverterComponent} from "./bitcoin-converter/bitcoin-converter.component";
+import {BitcoinDiagramComponent} from "./bitcoin-diagram/bitcoin-diagram.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "bitcoin-details",component:BitcoinDetailsComponent},
+  {path: "bitcoin-converter",component:BitcoinConverterComponent},
+  {path: "bitcoin-diagram",component:BitcoinDiagramComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
